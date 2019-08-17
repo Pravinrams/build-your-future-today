230df72e8b2af4b9c3cfc689f854df99829ed2bd
Jeffrey-A <almanzarortizjeffrey@gmail.com>
2019-08-17 01:00:49 -0400

    Nav component initialized

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navigation from "./components/nav/Nav";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Word</h1>
+      <Navigation />
     </div>
   );
 }
---
+++ src/components/nav/Nav.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./nav.css";
+
+class Navigation extends React.Component {
+  render() {
+    return <h1>Hello</h1>;
+  }
+}
+
+export default Navigation;
