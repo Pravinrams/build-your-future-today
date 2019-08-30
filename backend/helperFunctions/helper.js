0bf16a67cf19a2215b4d308dd97352137dd178bc
Jeffrey-A <almanzarortizjeffrey@gmail.com>
2019-08-30 18:47:17 -0400

    define helper functions

---
+++ backend/helperFunctions/helper.js
@@ -0,0 +1,18 @@
+function createTaskQuery(name, description, completed, goalid) {
+    // do something here
+}
+
+function createGoalQuery(name, description,startdate,enddatecompleted,userid) {
+    // do something here
+}
+
+function registerUserQuery(name, description, completed, goalid) {
+    // do something here
+}
+
+exports.helpers = {
+    createGoalQuery,
+    createTaskQuery,
+    registerUserQuery,
+}
+
--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,42 @@
 const express = require("express");
-const db = require('./database/database');
+const db = require("./database/database");
 const cors = require("cors"); // for doing axios call without problems
 const bodyParser = require("body-parser"); // for have access to post request res.body
-
+const helpers = require('./helperFunctions/helper');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 db.connect()
- .then(()=>{
-     console.log("Database connected")
-     db.query('select * from tasks', (err, res) => {
-        console.log(res.rows);
-        db.end();
-      });
- })
- .catch((err) =>{
-     console.log('error connecting to db')
- })
+  .then(() => {
+    console.log("Database connected");
+    db.query("select * from goals", (err, res) => {
+      console.log(res.rows);
+      db.end();
+    });
+  })
+  .catch(err => {
+    console.log("error connecting to db");
+  });
 
 app.get("/tasks", (req, res) => {
   res.send("will send tasks");
 });
 
+app.post("/register", (req, res) => {
+    console.log("registering users")
+});
+
 app.get("/goals", (req, res) => {
   res.send("will send goals");
+});
+
+app.post("/createGoal", (req, res) => {
+  console.log("creating goals");
+});
+
+app.post("/createTask", (req, res) => {
+  console.log(req.body);
 });
 
 app.listen(3000, () => {
