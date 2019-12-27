915d9aa1ee06bb1691e9ce6f967581375b40aaca
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2019-12-27 17:55:59 -0500

    change file structure

--- database/database.js
+++
@@ -1,8 +0,0 @@
-const { Pool, Client } = require("pg");
-const connectionString = "postgres://postgres:password@localhost:5432/postgres";
-
-const db = new Client({
-  connectionString: connectionString
-});
-
-module.exports = db;
